a05742c69d0e935f0557b2f4966c6b7326a2615e
nedozrel <dixi.starodubtsev@gmail.com>
2021-11-05 22:21:27 +0700

    Создана основная структура программы

--- main.py
+++ main.py
@@ -1,7 +1,24 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.chrome.options import Options as chrome_options
 import time
 
-url = str(input('Введите ссылку на mint: '))
-driver = webdriver.Chrome()
-driver.get(url)
+
+def get_options():
+    options = chrome_options
+    return options
+
+
+def main():
+    url = str(input('Введите ссылку на mint: '))
+    if url.find('binance') != -1:
+        driver = webdriver.Chrome()
+        driver.get(url)
+        time.sleep(10)
+    else:
+        print('Ссылка не является ссылкой на сайт Binance!')
+
+
+if __name__ == '__main__':
+    main()
+    input('Нажмите Enter для завершения работы программы')
