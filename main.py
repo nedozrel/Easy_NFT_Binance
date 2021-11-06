24779ad97d99f40ff22230f3ff5cfb36a1c3bc30
nedozrel <dixi.starodubtsev@gmail.com>
2021-11-06 17:28:29 +0700

    Добавлен функционал логгина по кукисам, обновлена структура программы

--- main.py
+++ main.py
@@ -2,35 +2,52 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.chrome.options import Options as chrome_options
 import time
+import json
 
 
 def get_options():
-    options = chrome_options
+    options = chrome_options()
     return options
 
 
-def main():
-    url = str(input('Введите ссылку на mint: '))
-    if url.find('binance') != -1:
-        driver = webdriver.Chrome()
-        driver.get(url)
-        try:
-            accept_terms_button = driver.find_element(value='button.css-1mtehst', by='css selector')
-            accept_terms_button.click()
-            time.sleep(0.2)
-        except Exception:
-            print('accept terms button is not found')
-            raise Exception
+def get_driver(options=None):
+    driver = webdriver.Chrome(options=options)
+    return driver
 
+
+def get_page(driver, url):
+    if url.find('binance') != -1:
         try:
-            mint_button = driver.find_element(value='button.css-18fem9b', by='css selector')
-            mint_button.click()
+            driver.get(url)
         except Exception:
-            print('mint button is not found')
-            raise Exception
+            print('Что то пошло не так :(')
     else:
         print('Ссылка не является ссылкой на сайт Binance!')
+    time.sleep(2)
 
+
+def save_cookies(driver):
+    with open('cookies.json', 'w') as file:
+        json.dump(driver.get_cookies(), file)
+
+
+def load_cookies(driver):
+    with open('cookies.json', 'r') as cookies_file:
+        cookies = json.load(cookies_file)
+    for cookie in cookies:
+        driver.add_cookie(cookie)
+    time.sleep(2)
+
+
+def main():
+    print('Дождитесь загрузки браузера и залогиньтесь на бинанс...')
+    url = 'https://www.binance.com/ru'
+    driver = get_driver(options=get_options())
+    get_page(driver, url)
+    load_cookies(driver)
+    driver.refresh()
+    input('Если вы уже залогинены нажмите Enter')
+    save_cookies(driver)
     input('Нажмите Enter для завершения работы программы')
 
 
